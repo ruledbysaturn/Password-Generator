84459e4ad8c180b851a076893012090a615cb338
Ruminate <inramiland@gmail.com>
2023-09-23 20:12:26 -0400

    added array for lower case characters

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var specialCharacters = [ '!','"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
-
+var lowerCaseCharacters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
