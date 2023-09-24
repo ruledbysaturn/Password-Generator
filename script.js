38d5fad8fd51734d23643def6d4e370030733cdb
Ruminate <inramiland@gmail.com>
2023-09-23 20:23:32 -0400

    added array for numeric characters

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 var specialCharacters = [ '!','"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 var lowerCaseCharacters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCaseCharacters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numericCharacters = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
